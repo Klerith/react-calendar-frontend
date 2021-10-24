c9c755f78f910f4592372147bf8d3298e7d0f4f5
Fernando Herrera <fernando.herrera85@gmail.com>
2021-10-24 16:08:56 -0400

    Punto inicial

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { CalendarApp } from './CalendarApp';
+
+import './styles.css';
+
+ReactDOM.render(
+    <CalendarApp />,
+  document.getElementById('root')
+);
