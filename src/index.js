a67a28b4cc3046ec88fa591e253a697154028bf6
Nicolas Heizenreder <heizenredernicolas@gmail.com>
2023-04-03 20:46:56 -0300

    manual service worker

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { CalendarApp } from './CalendarApp';
+import React from "react";
+import ReactDOM from "react-dom";
+import { CalendarApp } from "./CalendarApp";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
-import './styles.css';
+import "./styles.css";
 
-ReactDOM.render(
-    <CalendarApp />,
-  document.getElementById('root')
-);
+ReactDOM.render(<CalendarApp />, document.getElementById("root"));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
